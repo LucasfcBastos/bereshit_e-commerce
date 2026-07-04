6045239ff80a21a45ed91738a46aec88587e76ee
Lucas LBF Furango <lucas.software.engineering@gmail.com>
2026-07-04 16:13:25 -0300

    An image and text are added to the landing page only when the user visits it

--- frontend/src/pages/home/LandingPage.jsx
+++ frontend/src/pages/home/LandingPage.jsx
@@ -1,13 +1,26 @@
-import HeaderTop from '../../components/header/HeaderTop.jsx'
+import HeaderTop from "../../components/header/HeaderTop.jsx";
+
+import Art from "../../assets/img/BMW.png";
 
 function LandingPage() {
     return (
         <div>
             <HeaderTop />
-            <main>
+
+            <main className="first page">
+                <section className="hero">
+                    <div className="hero-text">
+                        <h1>O CARRO CERTO MUDA TUDO.</h1>
+                        <h1>ESCOLHA COM CONFIANÇA.<br/>DIRIJA COM TRANQUILIDADE.</h1>
+                        <p>Os melhores veículos, cuidadosamente selecionados<br/>para levar você mais longe.</p>
+                    </div>
+                    <div className="hero-image">
+                        <img src={Art} alt="BMW" />
+                    </div>
+                </section>
             </main>
         </div>
-    )
+    );
 }
 
 export default LandingPage;
